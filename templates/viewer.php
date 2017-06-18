5287764ff9406c662bf482f7930c138d88901fec
Sergio Souza <sergio.henrique.souza@gmail.com>
2017-06-18 11:58:40 -0300

    Create viewer.php

---
+++ templates/viewer.php
@@ -0,0 +1,16 @@
+<?php
+style('msofficeviewer', 'style');
+?>
+
+<div id="app">
+        <div id="app-navigation">
+                <?php print_unescaped($this->inc('navigation/index')); ?>
+                <?php print_unescaped($this->inc('settings/index')); ?>
+        </div>
+
+        <div id="app-content">
+                <div id="app-content-wrapper">
+                        <?php print_unescaped($this->inc('content/index')); ?>
+                </div>
+        </div>
+</div>
