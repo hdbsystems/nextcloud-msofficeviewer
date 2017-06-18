3e479370d2734520f68d127cbd09aa97a22a5f23
Sergio Souza <sergio.henrique.souza@gmail.com>
2017-06-18 11:55:48 -0300

    Update PageController.php

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,31 +1,63 @@
 <?php
+
+/**
+ * Nextcloud - MS Office Documents Viewer App
+ *
+ * @author Sergio Souza
+ * @copyright 2017 Sergio Souza sergio.henrique.souza@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 namespace OCA\MSOfficeViewer\Controller;
 
 use OCP\IRequest;
-use OCP\AppFramework\Http\TemplateResponse;
-use OCP\AppFramework\Http\DataResponse;
+use OCP\IURLGenerator;
 use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Http\ContentSecurityPolicy;
 
-class PageController extends Controller {
-	private $userId;
+class DisplayController extends Controller {
 
-	public function __construct($AppName, IRequest $request, $UserId){
-		parent::__construct($AppName, $request);
-		$this->userId = $UserId;
-	}
+        private $urlGenerator;
 
-	/**
-	 * CAUTION: the @Stuff turns off security checks; for this page no admin is
-	 *          required and no CSRF check. If you don't know what CSRF is, read
-	 *          it up in the docs or you might create a security hole. This is
-	 *          basically the only required method to add this exemption, don't
-	 *          add it to any other method if you don't exactly know what it does
-	 *
-	 * @NoAdminRequired
-	 * @NoCSRFRequired
-	 */
-	public function index() {
-		return new TemplateResponse('msofficeviewer', 'index');  // templates/index.php
-	}
+        public function __construct($AppName, IRequest $request, IURLGenerator $urlGenerator){
+                parent::__construct($AppName, $request);
+                $this->urlGenerator = $urlGenerator;
+        }
+
+        /**
+         * @NoAdminRequired
+         * @NoCSRFRequired
+         */
+        public function previewDocumet() {
+                    $params = [
+                'urlGenerator' => $this->urlGenerator
+            ];
+                $response = new TemplateResponse('msofficeviewer', 'viewer', $params, 'blank');  // templates/viewer.php
+
+                $policy = new ContentSecurityPolicy();
+                $policy->addAllowedChildSrcDomain('\'self\'');
+                $policy->addAllowedFontDomain('data:');
+                $policy->addAllowedImageDomain('*');
+                $policy->allowEvalScript(false);
+                $response->setContentSecurityPolicy($policy);
+
+                return $response;
+
+        }
 
 }
